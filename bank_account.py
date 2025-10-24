3a54e12c828f841cc81462dc845d2e990b401e48
rita51127 <rita.5111997@gmail.com>
2025-10-24 18:55:23 -0400

    example

---
+++ bank_account.py
@@ -0,0 +1,34 @@
+"""
+Task: To implement bank account
+Bank account functionalities: deposit, withdrawal, and balance information
+"""
+
+# comment
+
+class BankAccount:
+
+    def __init__(self):
+      self._balance = 0.0
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def withdraw(self, amount):
+        if amount > self._balance:
+            print(f"insufficient funds")
+            return
+
+        self._balance -= amount
+
+    def show_balance(self):
+        print(f"Your balance is: {self._balance:,.2f}$")
+
+
+
+
+acc1 = BankAccount()
+acc1.deposit(100)
+acc1.withdraw(1000)
+acc1.show_balance()
+
+acc2 =BankAccount()
---
+++ main.py
@@ -0,0 +1,16 @@
+print('hello')
+
+class Dog:
+    def __init__(self, name, breed):
+        self._name = name
+        self._breed = breed
+    
+    def bark(self):
+         print(f"The {self._breed} named {self._name} says woof!") 
+
+dog1 = Dog("wolf", "GSD")
+
+dog2 = Dog("Pup", "Shitzu")
+
+dog1.bark()
+dog2.bark()
